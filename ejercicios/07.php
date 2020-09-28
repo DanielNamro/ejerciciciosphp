3eb3fc45cde2d395457fcc3c115e2c4cc6360063
DanielNamro <danielnamrofpb@gmail.com>
2020-09-28 18:06:21 +0100

    ejercicio7

---
+++ ejercicios/07.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>ejemplo</title>
+</head>
+</head>
+
+<body>
+<?php
+$paises = ["Italia", "España" , "Francia", "Andorra"];
+
+foreach ($paises as $salida) {
+    echo "$salida" , " ";
+}
+
+?>
+</body>
+
+</html>
