a59aa5d1970e4d9afa5921017fd34bb8b56ac4f4
DanielNamro <danielnamrofpb@gmail.com>
2020-09-30 16:53:20 +0100

    ejercicio14

--- ejercicios/14.php
+++ ejercicios/14.php
@@ -8,12 +8,17 @@
 
 <body>
     <h2>Formulario</h2>
-    <form method="POST" action="14.php">
-    <input type="checkbox" name="Suma" value="Suma" > Suma <br>
-    <input type="checkbox" name="Resta" value="Resta" > Resta <br>
-    <input type="checkbox" name="Multiplicacion" value="Multiplicacion" > Multiplicacion <br>
-    <input type="checkbox" name="Division" value="Division" > Division <br>
-        <input type="submit" value="enviar">
+    <form method="POST" action="14action.php">
+    <select name="signo">
+           <option>+</option>
+           <option>-</option>
+           <option>*</option>
+           <option>/</option>
+       </select>
+       <input type="text" name="num1">
+       <input type="text" name="num2">
+
+       <input type="submit" value="enviar">
     </form>
 
 
