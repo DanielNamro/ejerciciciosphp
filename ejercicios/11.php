a62766b7b984d235f7165749b211d421f2829022
DanielNamro <danielnamrofpb@gmail.com>
2020-09-30 15:19:22 +0100

    ejercicio11

---
+++ ejercicios/11.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html><head>
+        <meta charset="UTF-8">
+        <title>ejemplo 11</title>
+</head><body>
+<form method="POST" action="11action.php">
+<label>Titulo</label><input type="text" value="" name="Titulo"><br>
+        <label>Autor</label><input type="text" value="" name="Autor"><br>
+        <label>Editorial</label><input type="text" value="" name="Editorial"><br>
+        <label>Paginas</label><input type="text" value="" name="Paginas"><br>
+        <input type="submit" value="enviar">
+</form>
+</body></html>
