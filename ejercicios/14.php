a2dec5028515fd298c88a706bf2b1175511bc451
DanielNamro <danielnamrofpb@gmail.com>
2020-09-30 16:30:20 +0100

    ejercicio14

---
+++ ejercicios/14.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>ejer14</title>
+</head>
+
+<body>
+    <h2>Formulario</h2>
+    <form method="POST" action="14.php">
+    <input type="checkbox" name="Suma" value="Suma" > Suma <br>
+    <input type="checkbox" name="Resta" value="Resta" > Resta <br>
+    <input type="checkbox" name="Multiplicacion" value="Multiplicacion" > Multiplicacion <br>
+    <input type="checkbox" name="Division" value="Division" > Division <br>
+        <input type="submit" value="enviar">
+    </form>
+
+
+</body>
+
+</html>
