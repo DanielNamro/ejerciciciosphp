89b50cb34068b41397cc1185399f02f2b43ad6fc
DanielNamro <danielnamrofpb@gmail.com>
2020-10-05 17:35:49 +0100

    ejercicio17

---
+++ ejercicios/17.php/app.php
@@ -0,0 +1,37 @@
+<?php
+
+class app{
+    public function __construct()
+    {
+    }
+  
+    public function run()
+    {
+      if (isset($_GET['method'])) {
+        $method = $_GET['method'];
+      } else {
+        $method = 'index';
+      }
+    
+   
+      $this->potenciados();  
+    }
+
+
+
+public function potenciados(){
+    $potencias = [];
+    
+
+    for ($i=1; $i <= 24 ; $i++) { 
+          $potencias[] = pow(2,$i);
+         // echo("$potencias  ");
+         }
+         include('views/index.php');
+    }
+
+public function index()
+{
+    include('views/index.php');}
+}
+?>
