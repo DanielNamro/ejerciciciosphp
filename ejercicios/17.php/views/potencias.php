2da50327221cf1148fab9cf88679ae06dd5e0bd6
DanielNamro <danielnamrofpb@gmail.com>
2020-10-05 18:07:04 +0100

    ejercicio17

---
+++ ejercicios/17.php/views/potencias.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <h1>OJALA FUNCIONES</h1>
+</head>
+<body>
+<ul>
+    <?php
+    foreach ($potencias  as $value) {
+    echo "<li>" , $value , "</li>"    ;
+    }
+    ?>
+</ul>
+
+</body>
+</html>
